db07bace74c990fb80560b2f83bcc6cd7f9f9a17
Ashwin2k3 <ashwin.2k3@gmail.com>
2023-12-10 23:32:43 +0530

    files Created

---
+++ Basics/Loops/Pattern_03.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+
+void triangle(int n) {
+	// Write your code here
+	for(int i = 1; i<=n ; i++){
+        for (int j = 1; j <= i; j++) {
+            cout << j ;
+          }
+		  cout<<endl;
+    }
+}
+
+int main(){
+	int n ;
+	n = 3;
+	triangle(n);
+
+}
